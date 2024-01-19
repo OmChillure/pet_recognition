a2c667837df6cea1eb86e40e454dcf88433b09a4
OmChillure <omchillure4@gmail.com>
2024-01-19 07:17:42 +0530

    skeleton of the web

---
+++ pet-web/src/Components/Navbar.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Style from "../Stylesheets/Navbar.module.css";
+
+const Navbar = () => {
+    return(
+        <header>
+            <div className={Style.headCont}>
+                
+            </div>
+        </header>
+    )
+}
+
+export default Navbar;
