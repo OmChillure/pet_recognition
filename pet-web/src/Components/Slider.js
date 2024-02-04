292623eaff187b205f8441cadb23cc7a48e785dd
OmChillure <omchillure4@gmail.com>
2024-02-04 21:59:32 +0530

    day12

--- pet-web/src/Components/Slider.js
+++ pet-web/src/Components/Slider.js
@@ -15,7 +15,7 @@ const YourComponent = () => {
     { id: 1, name: 'Pets world', image: pets_world, description: 'Expert Staff having the knowledge and expertise in assisting customers with pet care advice, nutrition, and choosing the right products.' },
     { id: 2, name: 'The pets park', image: pets_park , description: ' The extensive selection of pet supplies, including food, toys, cages, bedding, and other accessories to meet the needs of different pets are available here .' },
     { id: 3, name: 'Pets at Home', image: pets_at_home, description: 'We ensure the health and well-being of the pets, such as regular check-ups, vaccinations, and a clean, sanitized environment.' },
-    { id: 4, name: 'Pets Junction', image: pets_junction, description: 'Pet variety : the diverse range of pets available such a dogs cats small animals birds reptiles and fish .The variety of pet supplies, food, toys, and accessories available in the store.' },
+    { id: 4, name: 'Pets Junction', image: pets_junction, description: 'Pet variety : the diverse range of pets available such a dogs cats small animals birds reptiles and fish .The variety of pet supplies and accessories available.' },
     { id: 5, name: 'Pets Master', image: pets_masster, description: 'We deal in pets and present our sincere eforts. Variety of animal supply and accessories dear to your pet are available ...' },
     // Add more products as needed
   ];
--- pet-web/src/pages/Recognizer.js
+++ pet-web/src/pages/Recognizer.js
@@ -1,11 +1,37 @@
-import React from 'react';
+import React, { useState } from "react";
+import axios from "axios";
 
-const Recognizer = () => {
-  return(
+const ImageClassifier = () => {
+  const [result, setResult] = useState(null);
+
+  const handleImageUpload = async (event) => {
+    const file = event.target.files[0];
+
+    if (file) {
+      const formData = new FormData();
+      formData.append('image', file);
+
+      try {
+        const response = await axios.post('http://localhost:5000/classify', formData);
+        setResult(response.data);
+      } catch (error) {
+        console.error('Error classifying image:', error);
+      }
+    }
+  };
+
+  return (
     <div>
-
+      <h2>Image Classification Page</h2>
+      <input type="file" onChange={handleImageUpload} />
+      {result && (
+        <div>
+          <p>Class: {result.class}</p>
+          <p>Confidence: {result.confidence}</p>
+        </div>
+      )}
     </div>
-  )
+  );
 };
 
-export default Recognizer;
+export default ImageClassifier;
