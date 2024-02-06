7e2265a7e7700b4b74e5b8c3275880ba472a6d94
OmChillure <omchillure4@gmail.com>
2024-02-06 23:16:38 +0530

    day12

--- pet-web/src/App.js
+++ pet-web/src/App.js
@@ -1,23 +1,25 @@
-import "./Stylesheets/Root.css"
+import "./Stylesheets/Root.css";
 import { BrowserRouter, Routes, Route} from "react-router-dom";
-import Home from "./pages/Home"
+import Home from "./pages/Home";
 import Navbar from "./Components/Navbar";
-import Recognizer from "./pages/Recognizer.js";
+import Minigame from "./pages/Minigame.js";
 import Shops from "./pages/Shops.js";
 import About from "./pages/About.js";
+import Recognizer from "./pages/Recognizer.js";
 
 function App() {
   return (
     <>
-    <BrowserRouter>
-    <Navbar />
-    <Routes>
-      <Route element = {<Home />} path="/" />
-      <Route element = {<Shops />} path = "/shops"/>
-      <Route element={<Recognizer />} path="/recognizer" /> 
-      <Route element = {<About />} path = "/about"/>
-    </Routes>
-    </BrowserRouter>
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route element={<Home />} path="/" />
+          <Route element={<Shops />} path="/shops" />
+          <Route element={<Minigame />} path="/minigame" /> 
+          <Route element={<About />} path="/about" />
+          <Route element={<Recognizer />} path="/recognizer" />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
--- pet-web/src/Components/Navbar.js
+++ pet-web/src/Components/Navbar.js
@@ -2,6 +2,8 @@ import React from "react";
 import Style from "../Stylesheets/Navbar.module.css";
 import { Link } from "react-router-dom";
 import logo from "../Assets/logo.png";
+import profile from "../Assets/temp.jpeg"
+import scanner from "../Assets/cam.jpeg"
 
 const Navbar = () => {
   return (
@@ -13,17 +15,18 @@ const Navbar = () => {
       </div>
       <nav>
         <ul className={Style.list}>
-          <li><Link to={"/"}>Home</Link></li>
-          <li><Link to={"/shops"}>Shops</Link></li> {/* Corrected path to "/shops" */}
-          <li><Link to={"/recognizer"}>Recognizer</Link></li>
-          <li><Link to={"/about"}>About</Link></li>
+          <li className={Style.homebtn}><Link to={"/"}>Home</Link></li>
+          <li className={Style.shopbtn}><Link to={"/shops"}>Shops</Link></li> {/* Corrected path to "/shops" */}
+          <li className={Style.gamebtn}><Link to={"/minigame"}> MiniGame</Link></li>
+          <li className={Style.aboutbtn}><Link to={"/about"}>About</Link></li>
         </ul>
       </nav>
       <div className={Style.buttons}>
-        <button className={Style.signIn}>Sign In</button>
-        <button className={Style.signUp}>Sign Up</button>
+        <Link to={"/recognizer"}>
+          <img className={Style.scanner} src={scanner} alt="Scanner element" />
+        </Link>
+        <img className={Style.profile} src={profile} alt="Profile element"></img>
       </div>
-      {/* Add your ham component here, assuming it is correctly implemented */}
     </header>
   );
 };
